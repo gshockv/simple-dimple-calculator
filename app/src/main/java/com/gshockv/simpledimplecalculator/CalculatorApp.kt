cb7f75ebc94522059ff8a15e20cf1cc55f24aa5e
Vitaly Gashock <gshockv@gmail.com>
2023-11-27 22:52:02 +0200

    Hilt as dependency

---
+++ app/src/main/java/com/gshockv/simpledimplecalculator/CalculatorApp.kt
@@ -0,0 +1,8 @@
+package com.gshockv.simpledimplecalculator
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CalculatorApp : Application() {
+}
--- app/src/main/java/com/gshockv/simpledimplecalculator/MainActivity.kt
+++ app/src/main/java/com/gshockv/simpledimplecalculator/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.gshockv.simpledimplecalculator.ui.Calculator
 import com.gshockv.simpledimplecalculator.ui.theme.SimpleDimpleCalcTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
